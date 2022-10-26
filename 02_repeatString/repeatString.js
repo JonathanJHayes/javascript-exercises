26a22c23af27103baf3589932e4e9462ae9767be
JonathanJHayes <jonathan@jjhayes.co.uk>
2022-10-26 10:27:30 +0100

    Finished repeatString assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+ 
+    newString = "";
+ 
+    if (num === 0) {
 
+    }
+    else if ( num < 0) {
+        newString = "ERROR";
+    }
+    else {
+        for (i = 1; i<= num; i++) {
+            newString = newString + string;
+        }
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
