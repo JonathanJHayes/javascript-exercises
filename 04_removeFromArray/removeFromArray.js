cfc8d3a09b697526f5e42ec69005d78aee7671e2
JonathanJHayes <jonathan@jjhayes.co.uk>
2022-10-26 11:16:01 +0100

    finished removeFromArray assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toBeRemoved) {
+   
+    for (i = 0; i < toBeRemoved.length; i++) {
+        
+        for (j = 0; j< array.length; j++) {
+            
+            if (array[j] === toBeRemoved[i]) {
+                array.splice(j, 1);
+               
+            }
+            else {
+                
+            }
+        }
+    }
+   
+    return array;
 
 };
 
