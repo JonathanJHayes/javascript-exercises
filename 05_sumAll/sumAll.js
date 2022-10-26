48fd65829cdce63325dfb834fb49860e9c84a957
JonathanJHayes <jonathan@jjhayes.co.uk>
2022-10-26 11:47:38 +0100

    Finished sumAll assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(numStart, numEnd) {
+    
+    if ( numStart > 0 && numEnd > 0 
+        && Number.isInteger(numStart) && Number.isInteger(numEnd)) {
+        if ( numStart < numEnd) {
 
+        numSum = 1;
+        for ( i = numStart; i < numEnd; i++) {
+            numSum = numSum + i + 1;  }
+        return numSum;
+        }
+
+        else if (numStart > numEnd) {
+            numSum = 1;
+            for (i = numEnd; i < numStart; i++) {
+                numSum = numSum + i + 1;
+            }
+            return numSum;
+        }
+    }
+
+    else {
+        return "ERROR";
+
+    }
 };
 
 // Do not edit below this line
